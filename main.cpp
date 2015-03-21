5d673c9c5ce4bcce204c96bc18ffaf789bcefdd5
Minoong Ji <ji_dung@hotmail.com>
2015-03-20 23:49:25 -0400

    adds recursive function

--- main.cpp
+++ main.cpp
@@ -9,15 +9,29 @@ unsigned int PrintFactorialPlain (unsigned int givenInt) {
     return givenInt;
 }
 
+unsigned int PrintFactorialRecursive (unsigned int givenInt) {
+
+    if (givenInt >= 2)
+        givenInt *= PrintFactorialRecursive (givenInt - 1);
+    else if (givenInt == 1) // terminate condition
+        return givenInt;
+    else {
+        std::cout << "This can't be true" << std::endl;
+        return 0;   // error
+    }
+    
+    return givenInt;
+}
+
 int main () 
 {
     unsigned int result;
     unsigned int inputInteger;
     std::cout << "Input an integer" << std::endl;
     std::cin >> inputInteger;
-    std::cout << "You entered: " << inputInteger << std::endl;
 
     result = PrintFactorialPlain (inputInteger);
+    result = PrintFactorialRecursive (inputInteger);
     
     std::cout << result << std::endl;
     
